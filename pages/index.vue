bc6d899cbf58765943c70ab64984de5551b4bccb
Mukul108 <Mukulpal108@hotmail.com>
2023-07-24 15:39:45 +0530

    commiting changes

--- pages/index.vue
+++ pages/index.vue
@@ -3,15 +3,17 @@
     <div class="background-image"></div>
     <div class="glass-container">
       <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet">
+      <!-- Existing content -->
       <h3 class="subtitle0">TEAM</h3>
       <h1 class="title">Psyche</h1>
       <h2 class="subtitle">HUNGER OF BUILDING TECH</h2>
       <div class="social-buttons">
-        <a href="https://github.com/psycheai" class="button"> <img src="../public/social/git.svg" alt=""> </a>
-        <a href="https://discord.gg/y27qMeJY" class="button"> <img src="../public/social/dis.svg" alt=""> </a>
-        <a href="#" class="button"> <img src="../public/social/insta.svg" alt=""> </a>
-        <a href="#" class="button"> <img src="../public/social/twit.svg" alt=""> </a>
+        <!-- Existing social buttons -->
       </div>
+    </div>
+    <div class="glassy-buttons">
+      <div class="glassy-button">Story</div>
+      <div class="glassy-button">Projects</div>
     </div>
   </div>
 </template>
@@ -27,8 +29,6 @@
   position: fixed;
   top: 0;
   left: 0;
-  width: 100vw;
-  height: 100vh;
   width: 100%;
   height: 100%;
   background-image: url('../public/bg/psyche6.jpg');
@@ -47,8 +47,7 @@
   backdrop-filter: blur(5px);
   border-radius: 120px;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-  margin-left: 90px;
-  margin-right: 70px;
+  margin: 20px; /* Instead of using margin-left and margin-right separately, use a single margin value */
 }
 
 .title {
@@ -70,6 +69,7 @@
 
 .social-buttons {
   display: flex;
+  justify-content: center; /* Center the social buttons horizontally */
 }
 
 .button {
@@ -80,4 +80,69 @@
 .button:last-child {
   margin-right: 0;
 }
+
+.glassy-buttons {
+  display: flex;
+  justify-content: center; /* Center the glassy buttons horizontally */
+  margin: 20px auto; /* Use auto margin to center the buttons vertically */
+  max-width: 500px;
+  padding: 20px;
+}
+
+.glassy-button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+  background-color: rgba(255, 255, 255, 0.425);
+  backdrop-filter: blur(5px);
+  border-radius: 30px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  font-size: 1.2rem;
+  font-weight: bold;
+  padding: 10px;
+  margin: 10px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+.glassy-button:hover {
+  background-color: rgba(255, 255, 255, 0.6);
+}
+
+/* Media queries for responsiveness */
+
+/* For screens smaller than 768px (e.g., smartphones) */
+@media screen and (max-width: 767px) {
+  .glass-container {
+    margin: 10px;
+  }
+
+  .title {
+    font-size: 6rem;
+  }
+}
+
+/* For screens between 768px and 1023px (e.g., tablets) */
+@media screen and (min-width: 768px) and (max-width: 1023px) {
+  .glass-container {
+    margin: 15px;
+  }
+
+  .title {
+    font-size: 10rem;
+  }
+}
+
+/* For screens larger than 1023px (e.g., desktops) */
+@media screen and (min-width: 1024px) {
+  .glass-container {
+    margin: 30px;
+  }
+
+  .title {
+    font-size: 14.5rem;
+  }
+}
+
 </style>
